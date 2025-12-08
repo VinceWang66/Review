8ee6aab7d851b38f34d5baa84d2f7d5e7c32be1d
Vince Wang <vince.wang@silkdigital.dev>
2025-12-08 09:34:04 +0800

    chore:使用NestCLI实现CRUD模块初始化

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { OrdersModule } from './orders/orders.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OrderItemsModule } from './order_items/order_items.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, PrismaModule, OrdersModule, CategoriesModule, OrderItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
